7e93320425f0d708781b65aa2703a351ebdc2b66
julian.rincon <julian.rincon@ceiba.com.co>
2018-11-01 20:48:42 -0500

    Arreglando servicios

--- Restaurante/src/main/java/co/com/eafit/servicio/IRestauranteServicio.java
+++
@@ -1,8 +0,0 @@
-package co.com.eafit.servicio;
-
-import co.com.eafit.dto.RestauranteDTO;
-
-public interface IRestauranteServicio {
-
-	public abstract void agregarRestaurante(RestauranteDTO restauranteDTO);
-}
--- Restaurante/src/main/java/co/com/eafit/utilidad/mapeo/IRestauranteMapper.java
+++
@@ -1,9 +0,0 @@
-package co.com.eafit.utilidad.mapeo;
-
-import co.com.eafit.dto.RestauranteDTO;
-import co.com.eafit.entidad.RestauranteEntity;
-
-public interface IRestauranteMapper {
-
-	public RestauranteEntity dtoRestauranteAEntity(RestauranteDTO restauranteDTO);
-}
